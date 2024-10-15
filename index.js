f66329d43cdebbc5443db82f26705a1ef7d7ec76
edogola4 <edogola4@gmail.com>
2024-10-15 22:43:36 +0300

    JS-Arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 
 // Write your code here
+
+// Create a variable called multiply set to an equation that will multiply the variables num1 and num2; the result of the multiplication should be 62.
+
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+console.log(multiply); // This will output 62
+
+// Create a variable called random that will generate a random integer greater than 0.
+const random = Math.floor(Math.random() * 100) + 1;
+console.log(random);
+
+
+// Create a variable called mod set to an equation that will calculate the remainder of dividing variable num3 by num4; the remainder should be 4.
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+console.log(mod); // This will output 4
+
+
+// Create a variable called max that finds the highest number in a set; the value returned should be 20.
+
+const max = Math.max(10, 20, 15, 5, 20);
+console.log(max);
